7ede396a4e465834275662eaf7ad9f5f94505a17
Shyam <40246847+ShyamKumar1@users.noreply.github.com>
2021-07-25 18:05:40 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import pyttsx3
+import PyPDF2
+book = open('The_monk_who_sold_his_car.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book, strict=False)
+pages = pdfReader.numPages
+print(pages)
+speaker = pyttsx3.init()
+for num in range(10, pages):
+    page = pdfReader.getPage(10)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
